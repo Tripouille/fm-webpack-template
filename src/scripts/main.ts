3b521104d0b12edf6e9fafa6e10499390072ef35
jgambard <jmichel.gambard@gmail.com>
2024-04-29 16:08:34 +0200

    setup html, ts, sass, fonts, images

---
+++ src/scripts/main.ts
@@ -0,0 +1,3 @@
+import "@/fonts/Inter-VariableFont_wght.ttf";
+import "@/images/favicon-32x32.png";
+import "@/styles/main.scss";
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/scripts/main.ts",
+  module: {
+    rules: [
+      {
+        test: /\.(woff2?|ttf|eot)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "fonts/[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  output: {
+    filename: "bundle-[hash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
